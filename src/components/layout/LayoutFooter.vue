9147a67626e17bf5e5b7174e7a47dd76324a9609
王志康 <3403057565@qq.com>
2022-04-14 18:21:24 +0800

    1.0.2 优化超链接跳转

--- src/components/layout/LayoutFooter.vue
+++ src/components/layout/LayoutFooter.vue
@@ -2,17 +2,18 @@
   <div id="layout-footer">
     <p style="color:var(--theme-footer-text);">Copyright © by ZYFX All Rights Reserved.</p>
     <div style="width:400px;margin: 0 auto;padding: 20px 0;color:var(--theme-footer-text)">
+      <img src="https://cdn.jsdelivr.net/gh/zyfx-wzk/Images/theme/record.png" style="float: left" alt="">
       <a target="_blank" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=36040202000256"
          style="text-decoration: none;height: 20px;line-height: 20px">
-        <img src="https://cdn.jsdelivr.net/gh/zyfx-wzk/Images/theme/record.png" style="float: left" alt="">
-        <p style="float: left;height: 20px;line-height: 20px;margin: 0 0 0 5px;">
+        <p style="float: left;height: 20px;line-height: 20px;margin-left:5px;">
           赣公网安备 36040202000256号
         </p>
       </a>
-      <a href="https://beian.miit.gov.cn/" target="_blank">津ICP备2021009073号-1</a>
+      <a href="https://beian.miit.gov.cn/" target="_blank">
+        津ICP备2021009073号-1</a>
     </div>
     <div style="color: #e1dfdd">
-      <a :href="motto.href">{{ motto.hitokoto }}———{{motto.from_who}}「{{ motto.from }}」</a>
+      <a target="_blank" :href="motto.href">{{ motto.hitokoto }}———{{ motto.from_who }}「{{ motto.from }}」</a>
     </div>
   </div>
 </template>
@@ -26,7 +27,7 @@ export default {
   data() {
     return {
       motto: {
-        "href":"https://hitokoto.cn/?uuid=e7edd354-edd1-4f20-afad-30074c948cb0",
+        "href": "https://hitokoto.cn/?uuid=e7edd354-edd1-4f20-afad-30074c948cb0",
         "uuid": "e7edd354-edd1-4f20-afad-30074c948cb0",
         "hitokoto": "路曼曼其修远兮，吾将上下而求索。",
         "from": "离骚",
@@ -42,11 +43,14 @@ export default {
       axios.get('https://v1.hitokoto.cn')
           .then((result) => {
             this.motto = result.data;
-            this.motto.href="https://hitokoto.cn/?uuid="+this.motto.uuid;
+            this.motto.href = "https://hitokoto.cn/?uuid=" + this.motto.uuid;
           })
           .catch((error) => {
             console.log(error);
           })
+    },
+    toHitokoto() {
+      window.open(this.motto.href);
     }
   }
 }
@@ -59,5 +63,15 @@ export default {
   margin: auto;
   font-size: 13px;
   background-color: rgba(255, 255, 255, 0.1);
+
+  a {
+    transition: .5s all ease;
+    filter: contrast(100%);
+  }
+
+  a:hover {
+    color: var(--theme-a-hover);
+    filter: contrast(150%);
+  }
 }
 </style>
